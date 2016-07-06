ee047392edab5138db1284229966d1195fa01dab
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2016-07-06 11:53:09 -0400

    Made changes as per PEP-257

--- scripts/dynamic_free_pool.py
+++ scripts/dynamic_free_pool.py
@@ -7,9 +7,11 @@ import time
 import sys
 from requests.auth import HTTPBasicAuth
 import ConfigParser, os
-#Release nodes : Takes node list as arguments and puts them back to free pool 
-#	         after given time				
+
+				
 def release_nodes(haas_url, haas_username, haas_password, statusfile, non_persistent_list,threshold_time):  
+	"""Release nodes : Takes node list as arguments and puts them back to free pool 	        after given time
+	"""
 	free_node_list = get_free_node_list(haas_url, haas_username, haas_password)
 	#Only these nodes should be updated in the file(either for project or for time)
 	nodes_to_update_infile = list(Set(non_persistent_list)-Set(free_node_list))
@@ -29,13 +31,13 @@ def release_nodes(haas_url, haas_username, haas_password, statusfile, non_persis
 		#free pool for time less than threshold value then update only 
 		#time in status file
 	        if compare_projects(project_in_file, project_in_haas) \
-					and canLive(new_time, threshold_time):
+					and can_live(new_time, threshold_time):
 			update_time_for_node(statusfile, node, old_time, new_time)
 		#See if the node is in the same project and if it is outside
 		#free pool for time greater than threshold value then release
 		#the node back to free pool
 		elif  compare_projects(project_in_file, project_in_haas)\
-					and not canLive(new_time, threshold_time):
+					and not can_live(new_time, threshold_time):
 		  	#Detaching nodes from networks
 			if networks!=[]:
 				detach_networks_from_node(haas_url, haas_username, haas_password, node,\
@@ -55,22 +57,22 @@ def release_nodes(haas_url, haas_username, haas_password, statusfile, non_persis
 			#the nodes if they are in free pool.
 			#Power save mode on :)	
 
-#Boolean Function to check if the projects are same 
 def compare_projects(project_in_file,project_in_haas):
+	"""Boolean Function to check if the projects are same." 
 	if project_in_file == project_in_haas:
 		return True
 	else:
 		return False		  
 
-#Boolean Function to check if node can live in the same project.
-def canLive(time, threshold_time):
+def can_live(time, threshold_time):
+	"""Boolean Function to check if node can live in the same project."""
 	if time < threshold_time:
 		return True
 	else:
 		return False
 
-#Reads current information from file for every node
 def get_project_and_time_for_node(statusfile, node):
+	"""Reads current information from file for every node."""
 	with open(statusfile,'r') as status_file:
 		for line in status_file:
 			if node in line:
@@ -79,8 +81,8 @@ def get_project_and_time_for_node(statusfile, node):
 				node_duration = node_status[2]
 	return (node_project, node_duration)	
 
-#Updates the time for a node in file
 def update_time_for_node(statusfile, node, old_time, new_time):
+	"""Updates the time for a node in file."""
 	lines = []
 	with open(statusfile,'r') as status_file:
                 for line in status_file:
@@ -95,8 +97,8 @@ def update_time_for_node(statusfile, node, old_time, new_time):
 			status_file.write(line)
 
 
-#Project will be changed in file to match the project in HaaS.
 def update_project_in_status_file(statusfile, node, new_project, old_project):
+	"""Project will be changed in file to match the project in HaaS."""
 	lines = []
         with open(statusfile,'r') as status_file:
                 for line in status_file:
@@ -110,11 +112,10 @@ def update_project_in_status_file(statusfile, node, new_project, old_project):
                 for line in lines:
                         status_file.write(line)
 
-#Queries current status from HaaS for a node.
 def get_node_current_info(haas_url, haas_username, haas_password, node):
-	'''
+	"""
 	Checks the current information for the node and returns a tuple consisting of (<project_name>,List of <{"network": network-name}>, NIC name)
-	'''
+	"""
 	url = haas_url + "node/" + node
 	resp = requests.get(url, auth = (haas_username,haas_password))
 	node_info = resp.json()
@@ -124,10 +125,10 @@ def get_node_current_info(haas_url, haas_username, haas_password, node):
 	networks = [{"network":v} for k,v in data.items() if k.startswith('vlan')]
 	return (node_project, networks, node_nic)	
 
-#Detach networks from nodes.
 def detach_networks_from_node(haas_url, haas_username, haas_password, node, networks, node_nic):
-	'''Takes a node ,list of {network, "network-name"}- networks attached to it, nic on which network is attached and detaches all the networks
-	'''
+	"""
+	Takes a node ,list of {network, "network-name"}- networks attached to it, nic on which network is attached and detaches all the networks
+	"""
 	url = haas_url + "node/" + node + "/nic/" + node_nic + "/detach_network"
 	for network in networks:
 		res = requests.post(url, json.dumps(network), auth = (haas_username,haas_password))
@@ -135,8 +136,11 @@ def detach_networks_from_node(haas_url, haas_username, haas_password, node, netw
 	return res	
 
 
-#Updates the file with project name as free and releases it back to free pool
 def release_from_project(haas_url, haas_username, haas_password, statusfile, node, project):	
+        """
+	Updates the file with project name as free and 
+	releases it back to free pool
+	"""
 	lines = []
         with open(statusfile,'r') as status_file:
                 for line in status_file:
@@ -151,8 +155,8 @@ def release_from_project(haas_url, haas_username, haas_password, statusfile, nod
 						node, project)
 	return response	
 		
-#Release a node from a project. 
 def release_node_from_project(haas_url, haas_username, haas_password, node, project):
+	"""Release a node from a project."""
 	haas_url = haas_url + 'project/' + project + '/detach_node'
 	body = {
 		"node" : node
@@ -160,17 +164,19 @@ def release_node_from_project(haas_url, haas_username, haas_password, node, proj
 	r = requests.post(haas_url, data = json.dumps(body), auth = (haas_username, haas_password))
 	return r
 	
-#Checks for free pool in the list and returns the list
-def get_free_node_list(haas_url, haas_user_name, haas_password):	
+def get_free_node_list(haas_url, haas_user_name, haas_password):
+	"""Checks for free pool in the list and returns the list."""
 	free_node_list =  urlparse.urljoin(haas_url,'free_nodes')
 	free_node_list = requests.get(free_node_list, auth = (haas_user_name, haas_password))
 	return free_node_list
 
 
-#For checking purpose
 if __name__ == "__main__":
+	"""Main function for calling rest of functions"""
 	try:
 		config = ConfigParser.SafeConfigParser()
+		#Leaving it for as of now and has to be replaced by another 
+		#location which HIL has access to.
 		config.read("/home/ravi/leasing.cfg")
 		node_list = [x.strip() for x in config.get('haas','node_list').split(',')]
 		threshold = int(config.get('haas','threshold'))
